2eccf2f8857feebb8cbdd23a69faad7221aacf47
Carrot_lord <lev.saldaev@gmai.com>
2026-05-10 12:44:49 +0300

    Add Graph class interface

---
+++ saldaev.lev/S3/Graph.hpp
@@ -0,0 +1,44 @@
+#ifndef GRAPH_HPP
+#define GRAPH_HPP
+#include "../common/vector.hpp"
+#include "HashTable.hpp"
+#include <string>
+
+namespace saldaev
+{
+  struct Graph
+  {
+  private:
+    struct Hasher
+    {
+      size_t operator()(const std::pair< std::string, std::string >) const;
+    };
+
+    struct KeyEqual
+    {
+      bool operator()(const std::pair< std::string, std::string >, const std::pair< std::string, std::string >) const;
+    };
+
+    HashTable< std::pair< std::string, std::string >, Vector< size_t >, Hasher, KeyEqual > edges;
+    Vector< std::string > vertexes;
+
+    void removeEdgesOf(std::string);
+
+  public:
+    Vector< std::string > vertices() const;
+    bool hasVertex(const std::string &name) const;
+    void addVertex(const std::string &name);
+
+    bool hasEdge(const std::string &from, const std::string &to) const;
+    void addEdge(const std::string &from, const std::string &to, size_t weight);
+    void removeEdge(const std::string &from, const std::string &to, size_t weight);
+
+    const Vector< std::pair< std::string, size_t > > outgoingEdges(const std::string &name) const;
+    const Vector< std::pair< std::string, size_t > > incomingEdges(const std::string &name) const;
+
+    void clearEdges();
+    void clear();
+  };
+}
+
+#endif
