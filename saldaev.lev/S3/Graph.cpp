ab81610d8ddd5bef920c9f96342d5a748efa1480
Carrot_lord <lev.saldaev@gmai.com>
2026-05-12 22:16:20 +0300

    Implement Graph vertex methods

--- saldaev.lev/S3/Graph.cpp
+++ saldaev.lev/S3/Graph.cpp
@@ -4,3 +4,28 @@ saldaev::Graph::Graph():
   edges(10, Hasher(), KeyEqual()),
   vertexes(0)
 {}
+
+saldaev::Vector< std::string > saldaev::Graph::vertices() const
+{
+  return vertexes;
+}
+
+bool saldaev::Graph::hasVertex(const std::string &name) const
+{
+  for (size_t i = 0; i < vertexes.getSize(); ++i) {
+    if (vertexes[i] == name) {
+      return true;
+    }
+  }
+  return false;
+}
+
+void saldaev::Graph::addVertex(const std::string &name)
+{
+  for (size_t i = 0; i < vertexes.getSize(); ++i) {
+    if (vertexes[i] == name) {
+      throw std::logic_error("ununique name");
+    }
+  }
+  vertexes.pushBack(name);
+}
