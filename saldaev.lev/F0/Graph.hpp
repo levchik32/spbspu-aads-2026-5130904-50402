d65cf386c597ebb9de189b197576cfe08463a3b2
Carrot_lord <lev.saldaev@gmai.com>
2026-05-16 00:17:38 +0300

    Implement graph interface

---
+++ saldaev.lev/F0/Graph.hpp
@@ -0,0 +1,72 @@
+#ifndef GRAPH_HPP
+#define GRAPH_HPP
+#include "../common/HashTable.hpp"
+#include "../common/vector.hpp"
+
+namespace saldaev
+{
+  template < class T, class S >
+  struct Graph
+  {
+  private:
+    struct Hasher
+    {
+      size_t operator()(const std::string &p) const;
+    };
+
+    struct KeyEqual
+    {
+      bool operator()(const std::string &st, const std::string &nd) const;
+    };
+
+    struct Edge;
+    struct Vertex
+    {
+      T data;
+      Vector< Edge * > outgoing_edges;
+      Vector< Edge * > incoming_edges;
+    };
+
+    struct Edge
+    {
+      S data;
+      Vertex *from;
+      Vertex *to;
+    };
+
+    HashTable< std::string, Vertex *, Hasher, KeyEqual > vertexes;
+    Vector< Edge * > edges;
+
+    void removeEdgesOf(Vertex *);
+    Vertex *findVertex(const std::string &id);
+
+  public:
+    Vector< std::string > vertexIds() const;
+    bool hasVertex(const std::string &id) const;
+    void addVertex(const T &data, const std::string &id);
+    void removeVertex(const std::string &id);
+    void setVertexData(const std::string &id, const T &data);
+    T getVertexData(const std::string &id) const;
+
+    Vector< std::pair< std::string, std::string > > edgesIds() const;
+    bool hasEdge(const std::string &from, const std::string &to) const;
+    void addEdge(const std::string &from, const std::string &to, const S &data);
+    void removeEdge(const std::string &from, const std::string &to);
+    void setEdgeData(const std::string &from, const std::string &to, const S &data);
+    S getEdgeData(const std::string &from, const std::string &to) const;
+
+    size_t indegree(const std::string &id) const;
+    size_t outdegree(const std::string &id) const;
+    size_t degree(const std::string &id) const;
+    const Vector< std::string > outgoingEdges(const std::string &id) const;
+    const Vector< std::string > incomingEdges(const std::string &id) const;
+
+    size_t countVertexes() const;
+    size_t countEdges() const;
+
+    void clearEdges();
+    void clear();
+  };
+}
+
+#endif
---
+++ saldaev.lev/common/HashTable.hpp
@@ -0,0 +1,95 @@
+#ifndef HASHTABLE_HPP
+#define HASHTABLE_HPP
+#include "../common/List.hpp"
+#include "../common/vector.hpp"
+#include <cstddef>
+#include <utility>
+
+namespace saldaev
+{
+  template < class Key, class Value, class Hash, class Equal >
+  struct HashTable
+  {
+  public:
+    struct Iterator
+    {
+      friend class HashTable;
+
+    public:
+      std::pair< Key, Value > &operator*();
+      std::pair< Key, Value > *operator->();
+
+      Iterator &operator++();
+      Iterator operator++(int);
+
+      bool operator==(const Iterator &other) const;
+      bool operator!=(const Iterator &other) const;
+
+    private:
+      Vector< List< std::pair< Key, Value > > > *buckets_;
+      size_t bucket_idx_;
+      typename List< std::pair< Key, Value > >::LIter list_iter_;
+
+      Iterator(Vector< List< std::pair< Key, Value > > > *b, size_t idx,
+               typename List< std::pair< Key, Value > >::LIter it);
+    };
+
+    struct ConstIterator
+    {
+      friend class HashTable;
+
+    public:
+      const std::pair< Key, Value > &operator*() const;
+      const std::pair< Key, Value > *operator->() const;
+
+      ConstIterator &operator++();
+      ConstIterator operator++(int);
+
+      bool operator==(const ConstIterator &other) const;
+      bool operator!=(const ConstIterator &other) const;
+
+    private:
+      const Vector< List< std::pair< Key, Value > > > *buckets_;
+      size_t bucket_idx_;
+      typename List< std::pair< Key, Value > >::LCIter list_iter_;
+
+      ConstIterator(const Vector< List< std::pair< Key, Value > > > *b, size_t idx,
+                    typename List< std::pair< Key, Value > >::LCIter it);
+    };
+
+    HashTable(const HashTable &other);
+    HashTable(HashTable &&other) noexcept;
+    HashTable(size_t slots, Hash hasher, Equal key_eq);
+    ~HashTable() = default;
+    HashTable &operator=(const HashTable &other);
+    HashTable &operator=(HashTable &&other) noexcept;
+
+    void add(Key k, Value v);
+    bool has(Key k) const noexcept;
+    Value get(Key k) const;
+    Value &at(Key k);
+    void remove(Key k);
+    void rewrite(Key k, Value v);
+
+    void rehash(size_t slots);
+    void clear() noexcept;
+    size_t size() const noexcept;
+    size_t bucket_count() const noexcept;
+    bool empty() const noexcept;
+    void swap(HashTable &other) noexcept;
+
+    Iterator begin();
+    Iterator end();
+    ConstIterator begin() const;
+    ConstIterator end() const;
+
+  private:
+    Vector< List< std::pair< Key, Value > > > data_;
+    Hash hasher_;
+    Equal key_eq_;
+    size_t slots_ = 0;
+    size_t elements_ = 0;
+  };
+}
+
+#endif
