78b0c635e955c275eae58dc89bd8538c5bc4621c
Carrot_lord <lev.saldaev@gmai.com>
2026-05-15 21:50:53 +0300

    Implement bind and cut commands

--- saldaev.lev/S3/main.cpp
+++ saldaev.lev/S3/main.cpp
@@ -182,3 +182,63 @@ void handleInbound(std::istream &in, std::ostream &out, GraphStorage &graphs)
     out << '\n';
   }
 }
+
+void handleBind(std::istream &in, std::ostream &out, GraphStorage &graphs)
+{
+  std::string gName;
+  in >> gName;
+  if (!(graphs.has(gName))) {
+    out << "<INVALID COMMAND>\n";
+    return;
+  }
+
+  saldaev::Graph *graph = graphs.get(gName);
+
+  std::string vName1, vName2;
+  size_t w = 0;
+  if (!(in >> vName1 >> vName2 >> w)) {
+    out << "<INVALID COMMAND>\n";
+    return;
+  }
+  if (!(graph->hasVertex(vName1))) {
+    graph->addVertex(vName1);
+  }
+  if (!(graph->hasVertex(vName2))) {
+    graph->addVertex(vName2);
+  }
+
+  graph->addEdge(vName1, vName2, w);
+}
+
+void handleCut(std::istream &in, std::ostream &out, GraphStorage &graphs)
+{
+  std::string gName;
+  in >> gName;
+  if (!(graphs.has(gName))) {
+    out << "<INVALID COMMAND>\n";
+    return;
+  }
+
+  saldaev::Graph *graph = graphs.get(gName);
+
+  std::string vName1, vName2;
+  size_t w = 0;
+  if (!(in >> vName1 >> vName2 >> w)) {
+    out << "<INVALID COMMAND>\n";
+    return;
+  }
+  if (!(graph->hasVertex(vName1)) || !(graph->hasVertex(vName2))) {
+    out << "<INVALID COMMAND>\n";
+    return;
+  }
+  if (!(graph->hasEdge(vName1, vName2))) {
+    out << "<INVALID COMMAND>\n";
+    return;
+  }
+
+  try {
+    graph->removeEdge(vName1, vName2, w);
+  } catch (const std::logic_error &) {
+    out << "<INVALID COMMAND>\n";
+  }
+}
