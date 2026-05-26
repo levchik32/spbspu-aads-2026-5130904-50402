7f9673ebdd48776f706108cc8fb050a67941c8d1
Carrot_lord <lev.saldaev@gmai.com>
2026-05-26 22:19:04 +0300

    Implement command processing cycle

---
+++ saldaev.lev/F0/main.cpp
@@ -0,0 +1,29 @@
+#include <fstream>
+#include <iostream>
+#include "comandHandlers.hpp"
+
+int main()
+{
+  using Command = void (*)(std::istream &, std::ostream &, saldaev::Baskets &, saldaev::Recipes &);
+  using Commands = saldaev::HashTable< std::string, Command, std::hash< std::string >, std::equal_to< std::string > >;
+
+  Commands commands(10, std::hash< std::string >(), std::equal_to< std::string >());
+
+  saldaev::Recipes recipes;
+  saldaev::Baskets baskets(5, std::hash< std::string >{}, std::equal_to< std::string >{});
+
+  std::string cmd = "";
+  while (std::cin >> cmd) {
+    if (commands.has(cmd)) {
+      commands.get(cmd)(std::cin, std::cout, baskets, recipes);
+    } else {
+      std::cout << "<INVALID COMMAND>\n";
+    }
+  }
+
+  auto it = baskets.begin();
+  while (it != baskets.end()) {
+    delete it->second;
+    ++it;
+  }
+}
